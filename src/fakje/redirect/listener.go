36e4cc81fd63c5961f3aff225379ae49e2322c4b
Krzysztof Wielogorski <kwielogorski@gmail.com>
2014-06-03 23:47:23 +0100

    added mysql update

--- src/fakje/redirect/listener.go
+++ src/fakje/redirect/listener.go
@@ -11,10 +11,15 @@ import (
 )
 
 var config Config
+var db *sql.DB
 var cache = map[string]string{}
 
 func main() {
 	config = getConfig()
+	db, _ = sql.Open("mysql", config.Db.User+":"+config.Db.Pass+"@/"+config.Db.Name)
+	// if err != nil {
+	// 	log.Printf("Error with db connection: %s", err.Error())
+	// }
 	http.HandleFunc("/", linkHandler)
 	http.ListenAndServe(":8088", nil)
 }
@@ -37,28 +42,31 @@ func linkHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("No redirection for: %s, cause: %s", rdct.slug, rdct.err), http.StatusNotFound)
 	} else {
 		http.Redirect(w, r, rdct.url, http.StatusMovedPermanently)
+		logRedirection(rdct)
 	}
 
+}
+
+func logRedirection(rdct Redirection) {
+	_, err := db.Exec("UPDATE link SET clicks=(clicks+1) WHERE slug = ?", rdct.slug)
+	if err != nil {
+		log.Printf("There was a problem during saving the click count.")
+	}
 }
 
 func getLink(rdct Redirection) Redirection {
 
 	url := cache[rdct.slug]
 	if url == "" {
-		log.Printf("Cache miss for %s", rdct.slug)
+		// log.Printf("Cache miss for %s", rdct.slug)
 	} else {
-		log.Printf("Found in cache: %s => %s", rdct.slug, url)
+		// log.Printf("Found in cache: %s => %s", rdct.slug, url)
 		rdct.url = url
 		return rdct
 	}
 
-	db, err := sql.Open("mysql", config.Db.User+":"+config.Db.Pass+"@/"+config.Db.Name)
-	if err != nil {
-		log.Printf("Error with db connection: %s", err.Error())
-	}
-
 	var redirection string
-	if err = db.QueryRow("SELECT url FROM link WHERE tag = ?", rdct.slug).Scan(&redirection); err != nil {
+	if err := db.QueryRow("SELECT url FROM link WHERE slug = ?", rdct.slug).Scan(&redirection); err != nil {
 		rdct.err = "Redirection doesn't exist"
 	} else {
 		cache[rdct.slug] = redirection
